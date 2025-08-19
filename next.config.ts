169871858a31328a0849fad42045518de7630b74
Sudip Das <sudip5428@gmail.com>
2025-08-19 18:18:25 +0530

    code commit for next config changes

--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   async redirects() {
-        return [
-          {
-            source: '/',
-            destination: '/dashboard',
-            permanent: true, // or false for temporary redirect
-          },
-        ];
+    return [
+      {
+        source: "/",
+        destination: "/dashboard",
+        permanent: true, // or false for temporary redirect
+      },
+    ];
+  },
+  experimental: {
+    optimizeCss: false,
   },
 };
 
