ea367c255e46a07b9dcb635cf55c405817bee301
Sudip Das <sudip5428@gmail.com>
2025-08-24 17:37:06 +0530

    code commit for new pages add

---
+++ src/app/password/[id]/page.tsx
@@ -0,0 +1,9 @@
+export default function ResetPasswordPage() {
+  return (
+    <div>
+      <h1>Reset Password</h1>
+      <p>Please follow the instructions to reset your password.</p>
+      {/* Add your reset password form or component here */}
+    </div>
+  );
+}
---
+++ src/app/password/page.tsx
@@ -0,0 +1,8 @@
+export default function PasswordPage() {
+  return (
+    <div>
+      <h1>Password Page</h1>
+      <p>This is the password page content.</p>
+    </div>
+  );
+}
